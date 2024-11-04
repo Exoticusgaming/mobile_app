4cafa75e675662106192e1d92ffb3e09e652dbbb
ExoticusGaming <exoticusgaming@gmail.com>
2024-11-04 16:43:49 -0600

    Update WelcomeScreen.js

--- frontend/Components/WelcomeScreen.js
+++ frontend/Components/WelcomeScreen.js
@@ -1,54 +1,105 @@
-import React from 'react';
-import { SafeAreaView, Text, Button, StyleSheet, View } from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Image, Text, Pressable, StyleSheet, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 export default function WelcomeScreen({ navigation, setIsLoggedIn }) {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+
+    getStorage()
+
+  }, [])
+
+  async function getStorage() {
+
+      const storedFirstName = await AsyncStorage.getItem("firstName");
+      const storedLastName = await AsyncStorage.getItem("lastName");
+      const storedEmail = await AsyncStorage.getItem("email");
+
+      if (storedEmail != null) {
+
+          setEmail(storedEmail)
+
+      }
+
+  }
+
   const handleLogout = async () => {
+
     await AsyncStorage.removeItem('token');
     setIsLoggedIn(false);
     navigation.reset({
       index: 0,
       routes: [{ name: 'Login' }],
+
     });
+
   };
 
-  const navigateToTestScreen = () => {
-    navigation.navigate('TestScreen');
+  const navigateToProfileScreen = () => {
+
+    navigation.navigate('Profile');
+
   };
-
-
 
   return (
+
     <SafeAreaView style={styles.container}>
-      <Text style={styles.headerText}>Welcome!</Text>
-      <View style={styles.buttonContainer} >
-        <Button title="New Screen" onPress={navigateToTestScreen} />
+
+      <Text style={styles.headerText}>Welcome, {email}!</Text>
+
+      <View>
+        <Image source={{uri:'https://www.worldanimalprotection.us/cdn-cgi/image/fit=cover,width=1376,format=auto/siteassets/images/wildlife/kangaroos/kangaroo-joey-getty.jpg'}} style={styles.profilePicture}/>
       </View>
-      <Button title="Logout" onPress={handleLogout} />
+      
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={navigateToProfileScreen}>View Profile</Pressable>
+        <Pressable style={styles.button} onPress={handleLogout}>Logout</Pressable>
+      </View>
+
     </SafeAreaView>
   );
 }
 
-
 const styles = StyleSheet.create({
+
+    profilePicture: {
+      width: 200,
+      height: 200,
+      alignSelf: 'center',
+      borderWidth: 5,
+      borderRadius: 950,
+    },
     container: {
         flex: 1,
         justifyContent: "center",
         padding: 16,
+        backgroundColor: '#eeeeee',
       },
       headerText: {
         fontSize: 24,
         marginBottom: 20,
         textAlign: "center",
+        fontWeight: "bold",
+        color: "black",
       },
-
       buttonContainer: {
         marginVertical: 10,
         alignSelf: 'center',
         width: '50%',
       },
+      button: {
+        borderColor: 'black',
+        padding: 5,
+        flex: 'center',
+        borderWidth: 1,
+        textAlign: "center",
+        margin: 5,
 
+    },
 
 });
